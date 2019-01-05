bc3a60cce496bc481a0e85a354147aab02f6d321
ranjayzheng <ranjayzheng@qq.com>
2019-01-05 16:30:59 +0800

    test: recreate the error

---
+++ __tests__/setups/v4-html-webpack-plugin/customPlugin.js
@@ -0,0 +1,16 @@
+const pluginName = "CustomPlugin";
+
+class CustomPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap(pluginName, compilation => {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tap(
+        pluginName,
+        resourceData => {
+          console.log(resourceData);
+        }
+      );
+    });
+  }
+}
+
+module.exports = CustomPlugin;
---
+++ __tests__/setups/v4-html-webpack-plugin/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CustomPlugin = require("./customPlugin");
+
+module.exports = {
+  entry: {
+    bundle: ["./app.js"],
+  },
+  output: {
+    path: __dirname + "/dist",
+  },
+  plugins: [
+    new webpack.DefinePlugin({ FOO: "'BAR'" }),
+    new HtmlWebpackPlugin(),
+    new CustomPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
